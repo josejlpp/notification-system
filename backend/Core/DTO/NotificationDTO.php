2214c6872f14e288827690fe4cb523e9abcfa835
José Luiz <josejlpp@hotmail.com>
2023-03-14 22:22:45 -0300

    remove create from core

--- backend/Core/DTO/NotificationDTO.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Core\DTO;
-class NotificationDTO
-{
-    public int $category;
-    public string $message;
-}
--- backend/Core/Publisher/ResponsePublisher.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Core\Publisher;
-
-class ResponsePublisher
-{
-    private string $status;
-}
